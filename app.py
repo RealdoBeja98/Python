9253f1fd1681ef0eae3653be0833149da2bcf399
Realdo98 <realdobeja@gmail.com>
2020-04-22 23:12:32 +0100

    Todo

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 #connecting to the database
-connection = sqlite3.connect("todo.db")
+connection = sqlite3.connect("todo.db", check_same_thread = False)
 
 #cursor
 crsr = connection.cursor()
@@ -22,15 +22,17 @@ crsr.execute(sql)
 
 @app.route('/')
 def index():
-    return render_template('index.html') 
+    crsr =  connection.execute("SELECT * from todo")
+    info_of_row = crsr.fetchall()
+    return render_template('index.html', info_of_row = info_of_row)
 
 @app.route('/add', methods=['POST'])
 def add():
     #todo = Todo(text=request.form['todoitem'], complete=False)
     get_new_todo = request.form['todoitem']
-    sql =  "INSERT INTO Todo (text, complete) VALUES (get_new_todo, False)"
+    sql=  "INSERT INTO Todo (text, complete) VALUES ('get_new_todo', False)"
     crsr.execute(sql)
-    crsr.commit()
+    connection.commit()
     return redirect(url_for('index'))
 
 
@@ -39,6 +41,11 @@ def add():
 
 
 
-connection.close()
+
+
 if __name__ == '__main__':
     app.run(debug = True)
+
+connection.close()
+
+
